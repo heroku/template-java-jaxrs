e147bf2cf07834d75978f9799287cdc6ab04170e
Ryan Brainard <brainard@heroku.com>
2012-03-06 20:31:51 -0800

    Allow Grizzly to serve static content and add index.html

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,8 @@
-import com.sun.grizzly.http.SelectorThread;
-import com.sun.jersey.api.container.grizzly.GrizzlyWebContainerFactory;
+import com.sun.grizzly.http.embed.GrizzlyWebServer;
+import com.sun.grizzly.http.servlet.ServletAdapter;
+import com.sun.jersey.spi.container.servlet.ServletContainer;
 
-import javax.ws.rs.core.UriBuilder;
 import java.io.IOException;
-import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,19 +12,22 @@ public class Main {
     private static final int DEFAULT_PORT = 9998;
 
     public static void main(String[] args) throws IOException {
-        final SelectorThread server = GrizzlyWebContainerFactory.create(getBaseUri(), getInitParams());
-        logger.log(Level.INFO, "Grizzly started on port: " + server.getPort());
+        final GrizzlyWebServer gws = new GrizzlyWebServer(getPort());
+
+        final ServletAdapter jerseyAdapter = new ServletAdapter(ClassLoader.getSystemResource("static").getFile());
+        jerseyAdapter.addInitParameter("com.sun.jersey.config.property.packages", "controllers");
+        jerseyAdapter.addInitParameter("com.sun.jersey.api.json.POJOMappingFeature", "true");
+        jerseyAdapter.setContextPath("/");
+        jerseyAdapter.setHandleStaticResources(true);
+        jerseyAdapter.setServletInstance(new ServletContainer());
+        gws.addGrizzlyAdapter(jerseyAdapter, new String[] {"/"});
+
+        gws.start();
+
+        logger.log(Level.INFO, "Grizzly started on port: " + gws.getSelectorThread().getPort());
     }
 
-    private static URI getBaseUri() {
-        final int port = System.getenv("PORT") != null ? Integer.parseInt(System.getenv("PORT")) : DEFAULT_PORT;
-        return UriBuilder.fromUri("http://localhost/").port(port).build();
-    }
-
-    private static Map<String, String> getInitParams() {
-        final Map<String, String> initParams = new HashMap<String, String>();
-        initParams.put("com.sun.jersey.config.property.packages", "controllers");
-        initParams.put("com.sun.jersey.api.json.POJOMappingFeature", "true");
-        return initParams;
+    private static int getPort() {
+        return System.getenv("PORT") != null ? Integer.parseInt(System.getenv("PORT")) : DEFAULT_PORT;
     }
 }
