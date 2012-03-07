0890de466ee38cbb816898e3220bdbd4dfd69bfc
Ryan Brainard <brainard@heroku.com>
2012-03-06 17:58:36 -0800

    update to latest and remove @Produces to allow user to specify

--- src/main/java/controllers/Steps.java
+++ src/main/java/controllers/Steps.java
@@ -5,8 +5,6 @@ import models.Step;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.List;
@@ -15,14 +13,12 @@ import java.util.List;
 public class Steps {
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public static List<Step> index() throws IOException, URISyntaxException {
         return Step.fromAllFiles();
     }
 
     @GET
     @Path("/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
     public static Step getStep(@PathParam("id") String id) throws IOException {
         return Step.fromFile(id);
     }
