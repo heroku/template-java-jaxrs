9b04c36a4ad817c278954450888fabd801f89846
Ryan Brainard <brainard@heroku.com>
2012-03-08 11:21:20 -0800

    add basic time tests

---
+++ src/test/java/models/TimeTest.java
@@ -0,0 +1,23 @@
+package models;
+
+import org.junit.Test;
+
+import java.util.TimeZone;
+
+import static org.junit.Assert.assertEquals;
+
+public class TimeTest {
+    
+    @Test
+    public void testDefaultTimezone() throws Exception {
+        final Time time = new Time();
+        assertEquals(TimeZone.getDefault().getDisplayName(), time.getTimezone());
+    }
+
+    @Test
+    public void testSpecifiedTimezone() throws Exception {
+        final TimeZone est = TimeZone.getTimeZone("EST");
+        final Time time = new Time(est);
+        assertEquals(est.getDisplayName(), time.getTimezone());
+    }
+}
