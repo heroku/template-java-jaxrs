9d66ddc3175d8b53c23ba9193b49e484b6072024
Ryan Brainard <brainard@heroku.com>
2012-03-08 11:38:04 -0800

    remove timezone support from code and place as enhancement for developers

--- src/main/java/services/TimeService.java
+++ src/main/java/services/TimeService.java
@@ -4,10 +4,8 @@ import models.Time;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.TimeZone;
 
 @Path("/time")
 @Produces(MediaType.APPLICATION_JSON)
@@ -16,12 +14,6 @@ public class TimeService {
     @GET
     public Time get() {
         return new Time();
-    }
-
-    @GET
-    @Path("/{timezone}")
-    public Time get(@PathParam("timezone") String timezone) {
-        return new Time(TimeZone.getTimeZone(timezone.toUpperCase()));
     }
 
 }
