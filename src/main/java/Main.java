afb2f397c990c9bbf60284a08288210c7c775438
Ryan Brainard <brainard@heroku.com>
2012-03-12 12:38:18 -0700

    Replace grizzly with jetty

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,46 @@
-import com.sun.grizzly.http.embed.GrizzlyWebServer;
-import com.sun.grizzly.http.servlet.ServletAdapter;
-import com.sun.jersey.spi.container.servlet.ServletContainer;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
 
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
+/**
+ *
+ * This class launches the web application in an embedded Jetty container.
+ * This is the entry point to your application. The Java command that is used for
+ * launching should fire this main method.
+ *
+ */
 public class Main {
 
-    private static final Logger logger = Logger.getLogger(Main.class.getName());
-    private static final int DEFAULT_PORT = 9998;
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws Exception{
+        String webappDirLocation = "src/main/webapp/";
 
-    public static void main(String[] args) throws IOException {
-        final GrizzlyWebServer gws = new GrizzlyWebServer(getPort());
+        // The port that we should run on can be set into an environment variable
+        // Look for that variable and default to 8080 if it isn't there.
+        String webPort = System.getenv("PORT");
+        if (webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
 
-        final ServletAdapter jerseyAdapter = new ServletAdapter(ClassLoader.getSystemResource("static").getFile());
-        jerseyAdapter.addInitParameter("com.sun.jersey.config.property.packages", "services");
-        jerseyAdapter.addInitParameter("com.sun.jersey.api.json.POJOMappingFeature", "true");
-        jerseyAdapter.setContextPath("/");
-        jerseyAdapter.setHandleStaticResources(true);
-        jerseyAdapter.setServletInstance(new ServletContainer());
-        gws.addGrizzlyAdapter(jerseyAdapter, new String[] {"/"});
+        Server server = new Server(Integer.valueOf(webPort));
+        WebAppContext root = new WebAppContext();
 
-        gws.start();
+        root.setContextPath("/");
+        root.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
+        root.setResourceBase(webappDirLocation);
 
-        logger.log(Level.INFO, "Grizzly started on port: " + gws.getSelectorThread().getPort());
+        // Parent loader priority is a class loader setting that Jetty accepts.
+        // By default Jetty will behave like most web containers in that it will
+        // allow your application to replace non-server libraries that are part of the
+        // container. Setting parent loader priority to true changes this behavior.
+        // Read more here: http://wiki.eclipse.org/Jetty/Reference/Jetty_Classloading
+        root.setParentLoaderPriority(true);
+
+        server.setHandler(root);
+
+        server.start();
+        server.join();
     }
 
-    private static int getPort() {
-        return System.getenv("PORT") != null ? Integer.parseInt(System.getenv("PORT")) : DEFAULT_PORT;
-    }
 }
